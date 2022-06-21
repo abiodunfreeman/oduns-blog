c81ad61f500b6d4c13f1d314ada56dce90545737
Odun Freeman <bornduringwar@gmail.com>
2022-06-21 17:44:20 -0400

    JSON  State updates on form change

--- manage-post-client/src/components/Form.js
+++ manage-post-client/src/components/Form.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Form = () => {
   // states
@@ -38,7 +38,9 @@ const Form = () => {
       };
     });
   };
-
+  useEffect(() => {
+    setJson(JSON.stringify(formData));
+  }, [formData]);
   //return
   return (
     <div>
