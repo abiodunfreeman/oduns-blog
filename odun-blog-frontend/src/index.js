86291401a043e30dd289c257cba57925c6c320ce
Odun Freeman <bornduringwar@gmail.com>
2022-06-21 01:42:43 -0400

    react skeleton setup

---
+++ odun-blog-frontend/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const getData = async () => {
+  const response = await fetch('http://localhost:5000/posts', { mode: 'cors' });
+  console.log(response);
+};
+getData();
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
